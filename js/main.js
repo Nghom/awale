fcd001e01917fa0b922961eebcf7135a2afe9451
achilep <pahonsibebeto@gmail.com>
2019-08-02 11:20:40 +0100

    al

---
+++ js/main.js
@@ -0,0 +1,18 @@
+Object p1{
+    plyer1;
+    var p=Array(12);
+    player2;
+    var n;
+    function add(id){
+        for(var i=id;i<;i++){
+            p(i%12)=p(i%12)+1;
+            
+        }
+        
+        for(var i=id;i<12;i++){
+            p[6-i]=p[6-i]+1;
+        }
+        p[id]=0;
+        return p[];
+    }
+    }
